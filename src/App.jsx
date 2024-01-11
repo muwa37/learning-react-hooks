d91d1fc07104f08aedee7aebcbacea7586400a7c
nuwa37 <yond41m3@icloud.com>
2024-01-11 16:34:58 +0600

    added new comps and new hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Input from "./components/Input";
+import Hover from "./components/Hover";
+
 
 function App() {
+ 
   return (
     <div className="App">
-    
+      <Input/>
+      <Hover/>
     </div>
   );
 }
